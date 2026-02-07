a765bac6c28fb1036004335c3b7f46cf7c3625ac
saljaoui <soufian2022ff@gmail.com>
2026-02-07 01:39:55 +0100

    start project again

---
+++ src/main/java/zone01/soufian/lets_play/LetsPlayApplication.java
@@ -0,0 +1,13 @@
+package zone01.soufian.lets_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zone01/soufian/lets_play/LetsPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package zone01.soufian.lets_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
